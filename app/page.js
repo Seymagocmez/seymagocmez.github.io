e96caad5984b125c30b516c20b3c9e0b30c6d170
Şeyma G <104601325+Seymagocmez@users.noreply.github.com>
2024-05-02 19:00:07 +0300

    some commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { personalData } from "@/utils/data/personal-data";
+
 import AboutSection from "./components/homepage/about";
 
 
@@ -8,22 +8,9 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
 
 export default async function Home() {
-  const blogs = await getData();
+ 
 
   return (
     <>
